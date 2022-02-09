6a39f67e5541c7488b42d93dfd6c3510ee0c5a58
Slaktish1 <93648378+Slaktish1@users.noreply.github.com>
2022-02-09 13:41:11 +0100

    Select color and test it

---
+++ app/src/main/java/com/example/d0020e_project/ColortestSearch.java
@@ -0,0 +1,140 @@
+package com.example.d0020e_project;
+
+import static org.opencv.imgproc.Imgproc.boundingRect;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class ColortestSearch implements Runnable {
+
+    // TENNISBALL = lower: (70, 100, 100), upper: (103, 255, 255)
+    //private final Scalar LIGHTGREEN = new Scalar( 70, 100, 100 );
+    //private final Scalar DARKGREEN = new Scalar( 103, 255, 255 );
+    // Normal green?
+    private Scalar LightColor;
+    private Scalar DarkColor;
+
+    private LinkedBlockingQueue<Mat> queue = new LinkedBlockingQueue<Mat>();
+    private Point currentLocation = new Point( -1, -1 );
+    private Point currentLocation2 = new Point( -1, -1 );
+    private boolean run = true;
+    private final int BOXWIDTH, frameHeight;
+    private int activeLoops = 0;
+
+    public ColortestSearch(int boxW, int frameH, Scalar LC, Scalar DC) {
+        this.BOXWIDTH = boxW;
+        this.frameHeight = frameH;
+        this.LightColor = LC;
+        this.DarkColor = DC;
+        new Thread( this ).start();
+    }
+
+    public Point getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public Point getSecondLocation() {
+        return currentLocation2;
+    }
+
+    public void addFrame( Mat frame ) {
+        try {
+            queue.put( frame );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    synchronized boolean isRunning() {
+        return run;
+    }
+
+    synchronized void stopLoop() {
+        run = false;
+    }
+
+    synchronized void increaseActiveloops(){
+        this.activeLoops++;
+    }
+
+    synchronized void decreaseActiveloops(){
+        if (activeLoops > 0){
+            this.activeLoops--;
+        }
+
+    }
+
+    public void run() {
+        Mat blurred, hsv, mask;
+        while (isRunning()) {
+            // block when que empty
+            Mat frame = null;
+            try {
+                // TODO: find better way for this
+                frame = queue.poll( 200, TimeUnit.MILLISECONDS );
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (frame != null) {
+                blurred = frame.clone();
+                hsv = new Mat();
+                mask = new Mat();
+                Imgproc.GaussianBlur( frame, blurred, new Size( 11, 11 ), 0 );
+                Imgproc.cvtColor( blurred, hsv, Imgproc.COLOR_BGR2HSV );
+
+                /* The main functions to track colour object. */
+                //trackColors(0) == LIGHTColor, trackColors(1) = DARKColor
+                Core.inRange( hsv, DarkColor, LightColor, mask );
+                Imgproc.erode( mask, mask, new Mat() );
+                Imgproc.dilate( mask, mask, new Mat() );
+
+                Mat temp = new Mat();
+                        mask.copyTo( temp );
+                List<MatOfPoint> contours = new ArrayList<>();
+                Mat heirarchy = new Mat();
+                Imgproc.findContours( temp, contours, heirarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE ); // all external contours
+
+                Point coordinates = new Point();
+                Point coordinates2 = new Point();
+                if (contours.size() > 1) {
+                    //the largest contour is found at the end of the contours vector
+                    //we will simply assume that the biggest contour is the object we are looking for.
+                    List<Mat> largestContourVec = new ArrayList<>();
+                    largestContourVec.add( contours.get( contours.size() - 1 ) );
+                    largestContourVec.add( contours.get( 0 ) );
+                    //largestContourVec.add( contours.get( contours.size() - 2 ) );
+
+                    //make a bounding rectangle around the largest contour then find its centroid
+                    //this will be the object's final estimated position.
+                    coordinates = boundingRect( largestContourVec.get( 0 ) ).middle();
+                    coordinates2 = boundingRect( largestContourVec.get( 1 ) ).middle();
+
+                    // this is just for development purposes
+                    this.currentLocation = coordinates;
+                    this.currentLocation2 = coordinates2;
+                }
+                hsv.release();
+                blurred.release();
+                mask.release();
+                temp.release();
+                heirarchy.release();
+                frame.release();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/d0020e_project/SelectcolorTestview.java
@@ -0,0 +1,192 @@
+package com.example.d0020e_project;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class SelectcolorTestview extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private JavaCameraView javaCameraView;
+    private LoopBox loopBox;
+    private Box[] boxes = new Box[9];
+    private SoundPlayer soundPlayer;
+    private Mat frame1;
+    private final Scalar WHITE      = new Scalar( 255,255,255,0 );
+    private final Scalar BLUE = new Scalar( 0,0,255 );
+    private final Scalar GREEN = new Scalar( 0,255,0 );
+    private final Scalar RED = new Scalar( 255,0,0 );
+    private Scalar LC;
+    private Scalar DC;
+    private SelectcolorTestview camAct = this;
+    private ColortestSearch searchThread;
+
+
+    BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(SelectcolorTestview.this) {
+        @Override
+        public void onManagerConnected(int status) {
+            if (status == BaseLoaderCallback.SUCCESS) {
+                javaCameraView.enableView();
+            } else {
+                super.onManagerConnected( status );
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Might need this in order to ask for camera permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 200);
+        }
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+
+        setContentView(R.layout.camera_activity);
+        javaCameraView = findViewById(R.id.javaCameraView);
+        javaCameraView.setCameraPermissionGranted();
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(SelectcolorTestview.this);
+
+        LC = getLC(getIntent().getStringExtra( "colorKey" ));
+        DC = getDC(getIntent().getStringExtra( "colorKey" ));
+
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        Button btnBack = findViewById( R.id.btnBack );
+        btnBack.setOnClickListener( v -> {
+            searchThread.stopLoop();
+            startActivity( new Intent(SelectcolorTestview.this, soundSelection.class));
+            finish();
+        } );
+
+    }
+
+    //    private final Scalar LIGHTGREEN = new Scalar(64, 255, 255, 0);
+    //    private final Scalar DARKGREEN  = new Scalar(29, 86, 6, 0);
+
+    public Scalar getDC(String colorKey){       //Sätter "lightcolor" variabeln passande till den knapp som klickats.
+        switch(colorKey){
+            case "Green":
+                return new Scalar(29, 86, 6, 0);
+            case "Blue":
+                return new Scalar(13, 19, 114, 0);
+            case "Orange":
+                return new Scalar(127, 64, 9, 0);
+            default:
+                return new Scalar(255, 255, 255, 0);
+        }
+    }
+
+    public Scalar getLC(String colorKey){
+        switch(colorKey){
+            case"Green":
+                return new Scalar(64, 255, 255, 0);
+            case "Blue":
+                return new Scalar(40, 255, 255, 0);
+            case "Orange":
+                return new Scalar(255, 205, 190, 0);
+            default:
+                return new Scalar(0, 0, 0, 0);
+        }
+    }
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        int BOXHEIGHT = height / 4;
+        int BOXWIDTH = (int) Math.ceil(width / 6);
+        searchThread = new ColortestSearch(BOXWIDTH, height, LC, DC);
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        // read first frame
+        frame1 = inputFrame.rgba();
+        // use when testing on (some) emulator's.
+        //Imgproc.cvtColor( frame1, frame1, Imgproc.COLOR_BGR2RGB );
+
+        /* Add the current frame to queue in search for object thread */
+        if (frame1 != null) {
+            searchThread.addFrame(frame1.clone());
+        }
+        // draw our sensor locations, this will be removed, we do not want to draw on every frame.
+        Point coordinate = searchThread.getCurrentLocation();
+        Point coordinate2 = searchThread.getSecondLocation();
+        // For development purposes we draw a circle around the tracked object
+        Imgproc.circle( frame1, coordinate, 20, WHITE );
+        Imgproc.circle( frame1, coordinate2, 20, WHITE );
+
+        // make the image not mirrored
+        Core.flip(frame1, frame1, 1);
+        return frame1;
+    }
+
+    public void releaseObjects() {
+        frame1.release();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        releaseObjects();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture){ }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseObjects();
+        if (javaCameraView != null){
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseObjects();
+        if(javaCameraView != null) {
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        String TAG = "CameraActivity";
+        super.onResume();
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV is working!");
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        } else {
+            Log.d(TAG,"Opencv is not working!");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
+        }
+    }
+}
